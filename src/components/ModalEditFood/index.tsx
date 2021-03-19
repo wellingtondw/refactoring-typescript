fe82a9f661a46e14cfdd570dfda86a08a97a0445
wellington <wellingtonslpbi@hotmail.com>
2021-03-19 10:54:57 -0300

    refactoring modal edit food

--- src/components/ModalEditFood/index.tsx
+++ src/components/ModalEditFood/index.tsx
@@ -4,49 +4,45 @@ import { FiCheckSquare } from 'react-icons/fi';
 import { Form } from './styles';
 import Modal from '../Modal';
 import Input from '../Input';
+import { FoodInput } from '../ModalAddFood'
 import { FormHandles } from '@unform/core';
 
-export type FoodInput = {
-  image: string;
-  name: string;
-  price: number;
-  description: string;
-}
-
-type ModalAddFoodProps = {
+type ModalEditFoodProps = {
   isOpen: boolean;
   setIsOpen: () => void;
-  handleAddFood: (data: FoodInput) => void;
+  editingFood: FoodInput;
+  handleUpdateFood: (data: FoodInput) => void;
 }
 
-function ModalAddFood({ setIsOpen, isOpen, handleAddFood }: ModalAddFoodProps) {
+function ModalEditFood({  isOpen, setIsOpen, editingFood, handleUpdateFood }: ModalEditFoodProps) {
   const formRef = createRef<FormHandles>()
 
+
   const handleSubmit = async (data: FoodInput) => {
-    handleAddFood(data);
+    handleUpdateFood(data);
     setIsOpen();
   };
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
-      <Form ref={formRef} onSubmit={handleSubmit}>
-        <h1>Novo Prato</h1>
+      <Form ref={formRef} onSubmit={handleSubmit} initialData={editingFood}>
+        <h1>Editar Prato</h1>
         <Input name="image" placeholder="Cole o link aqui" />
 
         <Input name="name" placeholder="Ex: Moda Italiana" />
         <Input name="price" placeholder="Ex: 19.90" />
 
         <Input name="description" placeholder="Descrição" />
-        <button type="submit" data-testid="add-food-button">
-          <p className="text">Adicionar Prato</p>
+
+        <button type="submit" data-testid="edit-food-button">
+          <div className="text">Editar Prato</div>
           <div className="icon">
             <FiCheckSquare size={24} />
           </div>
         </button>
       </Form>
     </Modal>
-  );
+  )
 }
 
-
-export default ModalAddFood;
+export default ModalEditFood;
